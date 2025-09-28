691682db191c8969afa0b2a66fdc477f13884925
darrinz <darrinz@umich.edu>
2025-09-28 09:27:47 -0400

    supabase shit

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,4 +1,4 @@
 -- Drop all tables in reverse order of creation to handle foreign key constraints
 DROP TABLE IF EXISTS user_meeting_responses;
 DROP TABLE IF EXISTS meetings;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS 
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,26 +1,20 @@
--- Users table
-CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL
-);
-
 -- Meetings table
 CREATE TABLE IF NOT EXISTS meetings (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id SERIAL PRIMARY KEY,
   date TEXT NOT NULL,
   title TEXT NOT NULL,
   description TEXT,
-  pretasks TEXT, --JSON array of questions
-  attendees TEXT -- JSON array of attendee names
+  pretasks jsonb, --JSON array of questions
+  attendees jsonb -- JSON array of attendee names
 );
 
 -- Join table between users and meetings with responses and prompts
 CREATE TABLE IF NOT EXISTS user_meeting_responses (
-  (meeting_id, uid) INTEGER PRIMARY KEY AUTOINCREMENT,
-  uid TEXT NOT NULL,
+  email TEXT NOT NULL,
   meeting_id INTEGER NOT NULL,
-  responses TEXT, -- JSON object
-  prompt TEXT, -- JSON object
-  FOREIGN KEY (meeting_id) REFERENCES meetings(id),
-  FOREIGN KEY (uid) REFERENCES users(name)
+  responses jsonb, -- JSON object
+  prompt jsonb, -- JSON object
+  PRIMARY KEY (meeting_id, email),
+  FOREIGN KEY (meeting_id) REFERENCES meetings(id)
 );
+
